2ae24396a3db812762a0957406b8aba3767d56d8
Filip Hanik <fhanik@pivotal.io>
2024-12-10 13:59:35 -0800

    Remove hard coded database values from the MockMvc integration tests

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/DefaultTestContext.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/DefaultTestContext.java
@@ -36,10 +36,7 @@ public @interface DefaultTestContext {
 @ImportResource(locations = {"file:./src/main/webapp/WEB-INF/spring-servlet.xml"})
 @PropertySource(value = "classpath:integration_test_properties.yml", factory = NestedMapPropertySourceFactory.class)
 class SpringServletTestConfig {
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
+
 }
 
 class TestClientAndMockMvcTestConfig {
